99d07ff1a4465f003d8b771afc23838b82034b9c
gras <parrotabhijeet@gmail.com>
2026-03-15 23:50:52 +0530

    Initial setup and routes setup

---
+++ dcfrontend/app/routes.ts
@@ -0,0 +1,33 @@
+import 
+{ 
+    type RouteConfig, 
+    index, 
+    prefix, 
+    route, 
+    layout 
+
+} from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx"),
+    route("search-certificate", ""),
+    route("certificate", ""),
+    ...prefix("recepient", [
+        route("login", ""),
+        route("register", ""),
+        layout("", [
+            route("account", ""),
+            route("certificates", "")
+        ])
+    ]),
+    ...prefix("issuer", [
+        route("login", ""),
+        route("register", ""),
+        layout("", [
+            route("account", ""),
+            route("certifications", ""),
+            route("certificates", ""),
+            route("approvals", "")
+        ])
+    ])
+] satisfies RouteConfig;
