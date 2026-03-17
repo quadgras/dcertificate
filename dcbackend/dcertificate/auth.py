5c5e08b84b62bb3d9729942421237bd510232824
gras <parrotabhijeet@gmail.com>
2026-03-17 21:11:27 +0530

    Added requirement.txt and login functionality for recepient accounts.

--- dcbackend/dcertificate/auth.py
+++ dcbackend/dcertificate/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import (Blueprint, request, make_response, current_app)
 from werkzeug.security import check_password_hash, generate_password_hash
 from dcertificate.db import get_db
 
@@ -12,19 +12,24 @@ bp = Blueprint('auth', __name__, url_prefix="/auth")
 #     fail = "false"
 
 # NOTE: unless stated explicitly,
-# all these APIs will accept formdata
-# and not JSON.
+# all these APIs will accept JSON
+# and not formdata.
 # The decision is made because
-# formdata supports file uploads too
-# while JSON does not.
-# File uploads may be needed in future.
+# JSON adds little bit of more
+# protection agains CSRF attacks
+# that can be implemented using web-forms.
+# Although, to accept files,
+# formdata may be used.
+
 
 @bp.post("/recepient/register")
 def recepient_register():
-    username = request.form.get("username") #request.json["username"]
-    password = request.form.get("password")
-    display_name = request.form.get("display_name")
-    if(not(username and password and display_name)):
+    try:
+        username = request.json["username"] #request.form.get("username")
+        password = request.json["password"]
+        display_name = request.json["display_name"]
+    except KeyError:
+    #if(not(username and password and display_name)): # use instead of KeyError while dealing with formdata
         return {
             "success": False,
             "message": "One or more from username, "
@@ -55,15 +60,69 @@ def recepient_register():
 
 @bp.post("/recepient/login")
 def recepient_login():
+    try:
+        username = request.json["username"]
+        password = request.json["password"]
+    except KeyError:
+        return {
+            "success": False,
+            "message": "Keys: username or password "
+                       "is missing from request data."
+        }
+    
+    db = get_db()
+    user = db.execute(
+        "SELECT * FROM recepient "
+        "WHERE username = ?;",
+        (username,)
+    ).fetchone()
 
-    return {}
+    if(not user):
+        return {
+            "success": False,
+            "message": "Username {} not found.".format(username)
+        }
+    
+    if(not check_password_hash(user['pasword'], password)):
+        return {
+            "success": False,
+            "message": "Invalid password."
+        }
+    
+    # At this stage, user is valid.
+    # Hence granting access.
+
+    res = make_response({
+        "success": True,
+        "message": "User logged in successfully."
+    })
+    
+    res.set_cookie(
+        key = "recepient_auth_token",
+        value = user['username'],  # setting JWT recommended
+        httponly = True,
+        samesite = 'none',
+        secure = (current_app.config['ENVIRONMENT'] == 'production') # enforce https on production
+    )
+
+    return res
+
+@bp.post("/recepient/logout")
+def recepient_logout():
+    res = make_response({
+        "success":True,
+        "message":"Logged out any active login."
+    })
+    res.delete_cookie('recepient_auth_token')
+    return res
 
 @bp.post("/issuer/register")
 def issuer_register():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    display_name = request.form.get("display_name")
-    if(not(username and password and display_name)):
+    try:
+        username = request.json["username"]
+        password = request.json["password"]
+        display_name = request.json["display_name"]
+    except KeyError:
         return {
             "success": False,
             "message": "One or more from username, "
--- dcbackend/readme.md
+++ dcbackend/readme.md
@@ -2,3 +2,7 @@ CURL command reference for submitting formdata while testing -
 ```
 curl -X POST http://localhost:5000/auth/recepient/register -d 'username=test2&password=test2_password&display_name=TEST2'
 ```
+CURL command for submitting json -
+```
+curl -X POST http://localhost:5000/auth/recepient/register -H "Content-Type: application/json" -d '{"username":"test1", "password":"test1_password", "display_name":"TEST1"}'
+```
