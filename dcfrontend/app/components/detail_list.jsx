e25b0d7ad7163bcdb23f42e42314cb071d783fcf
gras <parrotabhijeet@gmail.com>
2026-05-05 00:22:00 +0530

    Added a new DetailsList component and used it to organize homepage

--- dcfrontend/app/components/detail_list.jsx
+++ dcfrontend/app/components/detail_list.jsx
@@ -2,31 +2,27 @@ import { useState } from "react";
 import styles from "./styles/detail_list.module.css";
 import { ChevronDown, ChevronRight } from "lucide-react";
 
-export default function DetailList({}){
-    const content = [
-        ["A", <p>Details of A</p>],
-        ["B", <p>Details of B</p>],
-        ["C", <p>Details of C</p>]
-    ];
+export default function DetailList({ content }) {
 
     const indexed_content = content.map((item, index) => [index, ...item]);
 
     const [activeKey, setActiveKey] = useState(null);
 
-    function toggle_detail(index){
-        if(index === activeKey)
+    function toggle_detail(index) {
+        if (index === activeKey)
             setActiveKey(null);
         else
             setActiveKey(index);
     }
 
     return <div className={styles.detail_list}>
-        {indexed_content.map(detail => <div className={styles.detail_element}>
-            <div className={styles.detail_title} onClick={()=>toggle_detail(detail[0])}>
-                {detail[0] === activeKey?<ChevronDown size={20}/>:<ChevronRight size={20}/>}
+        {indexed_content.map(detail => <div className={styles.detail_element} onClick={() => toggle_detail(detail[0])}>
+            <div className={styles.detail_title}>
                 {detail[1]}
+                {detail[0] === activeKey ? <ChevronDown size={20} /> : <ChevronRight size={20} />}
             </div>
-            {(detail[0] === activeKey) && detail[2]}
+
+            {(detail[0] === activeKey) && <div className={styles.detail_content}>{detail[2]}</div>}
         </div>)}
     </div>
 }
--- dcfrontend/app/routes/home.jsx
+++ dcfrontend/app/routes/home.jsx
@@ -6,49 +6,45 @@ import DetailList from "../components/detail_list";
 export default function Home() {
   return <>
     <header>
-      <DetailList />
       <DcertificateLogo />
       <h1>DCERTIFICATE</h1>
       <p>
-        Web application for 
+        Web application for
         issuing, approving and receiving
         digital certificates.
       </p>
     </header>
-    <article className={styles.about}>
-      <h1>Usecases</h1>
-      <div className={styles.infoLayout}>
-        <h2>Issuers & Approvers</h2>
-        <p>
-          Individuals and organizations
-          can create issuer-accounts to
-          launch and approve certifications.
-        </p>
+    <div className={styles.about}>
+      <DetailList content={[
+        [
+          'Issuers & Approvers',
+          `Individuals and organizations
+            can create issuer-accounts to
+            launch and approve certifications.`
+        ],
+        [
+          'Recipients',
+          `Individuals and organizations
+            can create recipient-account to
+            receive certificates from issuers.`
+        ],
+        [
+          'Third Parties',
+          `Third parties (including those that
+            don't have account on this platform)
+            like employers, educational institutions, etc.
+            can view and verify certificates
+            given by a recepient entity.`
+        ]
+      ]} />
 
-        <h2>Recipients</h2>
-        <p>
-          Individuals and organizations
-          can create recipient-account to
-          receive certificates from issuers.
-        </p>
-
-        <h2>Third Parties</h2>
-        <p>
-          Third parties (including those that
-          don't have account on this platform)
-          like employers, educational institutions, etc.
-          can view and verify certificates
-          given by a recepient entity.
-        </p>
-      </div>
-    </article>
-    <footer>
-      &copy;2026, Abhijeet Verma
-      <br />
-      This is an educational project.
+      <p>This is an educational project.
       The <a href="https://github.com/quadgras/dcertificate">source code</a> is
       available on Github
-      under terms of MIT Licence.
+      under terms of MIT Licence.</p>
+    </div>
+    <footer>
+      &copy;2026, Abhijeet Verma
     </footer>
     <RootNavigaion />
   </>;
